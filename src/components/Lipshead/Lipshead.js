b207126d6b0680a8189de695f27fd1857216241d
maxxflyer <maxxflyer@gmail.com>
2021-02-28 13:40:10 +0100

    Add files via upload

---
+++ src/components/Lipshead/Lipshead.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Logo from '../Logo/Logo';
+import Wing from '../Wing/Wing';
+import './css.css';
+class Lipshead extends Component {
+
+    render(){
+        return ( 
+        <div id="lipshead" opacity={1.0}>
+            <Logo  update={this.props.update}></Logo>
+            <Wing update={this.props.update}></Wing>  
+        </div>
+        );
+    }
+}
+export default Lipshead
