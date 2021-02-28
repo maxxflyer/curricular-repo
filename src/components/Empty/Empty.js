bce0880ffe2cd1ef011bff1209b8fd4ec2bb5a74
maxxflyer <maxxflyer@gmail.com>
2021-02-28 13:33:59 +0100

    Add files via upload

---
+++ src/components/Empty/Empty.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import './css.css';
+
+
+class Empty extends Component {
+   
+    render(){
+        
+        return ( 
+           <></>
+        );
+        
+    }
+
+
+}
+
+
+
+
+export default Empty
